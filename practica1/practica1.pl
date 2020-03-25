dd2a11190a1968301f14523c5090595ad13c4fe6
jesus.vallejo.collados@gmail.com <jesus.vallejo.collados@gmail.com>
2020-03-25 11:53:55 +0100

    hasta level todo bien

--- practica1/practica1.pl
+++ practica1/practica1.pl
@@ -11,13 +11,20 @@ igual(X,X).
 mayorZero(s(0)).               
 mayorZero(s(X)) :- mayorZero(X).
 
+append1([],Ys,Ys).
+append1([X|Xs],Ys,[X|Zs]) :-
+	append1(Xs,Ys,Zs).
 
-lengthEq(X,Y):-lengthEqCom(X,Y,0,0).
+suma(0, X, X):- nat(X).
+suma(s(X),Y,s(Z)):-suma(X,Y,Z).
+
+
+lengthEq(X,Y):-lengthEqCom(X,Y,0,0).% devuelve true si las dos listas tienen la misma longitud
 lengthEqCom([],[],_,_).
-lengthEqCom([A|B],[C|D],Z,P):- igual(Z,P),lengthEqCom(B,D,s(Z),s(P)).
+lengthEqCom([_|B],[_|D],Z,P):- igual(Z,P),lengthEqCom(B,D,s(Z),s(P)).
 
 
-array([[_]]).
+array([[_]]). % comprueba que las listas del edificio sean listas de listas 
 array([[_]|[_]]).
 array([[_|_]|[_|_]]).
 %------------------------  basic building type --------------------------------
@@ -26,7 +33,7 @@ basic_building1([]). % write_string("fin basic\n"),
 basic_building1([X|T]) :- basic_building_comp(X,T). % write_string("iteracion piso\n"),
 basic_building_comp([],K):-basic_building1(K).% write_string("fin piso, siguin\n"),
 basic_building_comp([Z|C],K):-nat(Z),basic_building_comp(C,K). % write_string("iteracion, vivienda\n"),
-%------------------------  basic building predicate tests ---------------------------
+%------------------------  basic building predicate tests ---------------------aaaaaaaws444
 %basic_building([[s(0)],[0]]).yes
 %basic_building([[s(0),0],[0,s(0)]]). yes
 %basic_building([[s(0),s(s(s(0)))],[0,s(s(0))]]). yes
@@ -51,23 +58,30 @@ checkLength([X,Y|T]):- lengthEq(X,Y),checkLength([Y|T]).
 %building([[0,s(0)],[0,0],[s(0),0],[0]]).   no
 
 %--------------------------  level predicate ---------------------------------------------------
-level(X,N,C):-level1(X,N,C,s(0)).
-level1(X,0,C,0):-write_string("C = "),write(C).
-level1([X|T],N,C,N):- level1(T,0,X,0).%write_string("C = "),write(C).
-level1([X|T],N,C,Z):-level1(T,N,X,s(Z)).
+level([],N,N).
+level([X|Y],N,C):-igual(N,s(0)),igual(C,X);(suma(Z,s(0),N),level(Y,Z,C)).
+
+
+
+
+
 
 %--------------------------  test level predicate ---------------------------------------------------
-% level([[ s(0), s(s(s(0))) ], [ 0, s(s(0)) ] ], s(s(0)), C). C=[ 0, s(s(0)) ]
+% X=[[ s(0), s(s(s(0))) ], [ 0, s(s(0)) ] ],level(X, s(s(0)), C). C=[ 0, s(s(0)) ]
  % level([[ s(0), s(s(s(0))) ], [ 0, s(s(0)) ] ], s(0), C).  C= [ s(0), s(s(s(0)))]
   % level([[ s(0), s(s(s(0))) ], [ 0, s(s(0)),s(s(0))],[ s(0), s(0)]], s(s(s(0))), C).  C= [ s(0), s(0)]
 
 %-----------------------------------------------------------------------------------------------
 
 %--------------------------  column predicate ---------------------------------------------------
-column(X,N,C):-column1(X,N,C).
-column1([],N,C):- write_string("C = "),write(C).
-column1([X|T],N,C):-column_comp(X,N,C,s(0),T).
-column_comp([X|Y],N,C,N,T):-column1(T,N,[C,X]).
-column_comp([X|Y],N,C,P,T):-column_comp(Y,N,C,s(P),T).
+%column(X,N,C).
+%columnRec([ListaPrincipal|RestoListaPrincipal],N,C) :- columnRec2(ListaPrincipal,N,C,S(0),RestoListaPrincipal).
+%columnRec2([],N,C,0,RestoListaPrincipal):- columnRec(RestoListaPrincipal,N,C).
+%columnRec2([ListaSecundaria|RestoListaSecundaria],N,C,N,RestoListaPrincipal):- columnRec(RestoListaPrincipal,N,[C,ListaSecundaria]).
+%columnRec2([ListaSecundaria|RestoListaSecundaria],N,C,P,RestoListaPrincipal):- columnRec2(RestoListaSecundaria,N,C,S(P),RestoListaPrincipal).
+ 
+
+
+
 
 %column([[ s(0),s(s(s(0)))], [0,s(s(0))]],s(0),C).
